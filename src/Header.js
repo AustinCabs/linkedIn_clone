3345116aec4e0abfc25ac7ec1293f4436effa22e
Aus_Cabs <46185867+AustinCabs@users.noreply.github.com>
2021-04-11 22:52:52 +0800

    add icon on navbar and avatar

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
-import HeaderOption from './HeaderOption'
 import HomeIcon  from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import HeaderOption from './HeaderOption'
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
  
 function Header() {
     return (
         <div className="header">
 
             <div className="header__left">
-                <img src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1617551382~hmac=8760e989c0e86e643a749f26b07a9fd6" alt="" />
+                <img src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1618149352~hmac=8d4a87ef3fe6bb1ebd68f12fc5f466eb" alt="" />
 
                 <div className="header__search">
                     <SearchIcon />
@@ -19,13 +22,18 @@ function Header() {
             </div>            
             
             <div className="header__right">
-                <HeaderOption title="Home" Icon={HomeIcon} />
-                <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+                <HeaderOption Icon={HomeIcon} title="Home"  />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"  />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"  />
+                <HeaderOption Icon={ChatIcon} title="Messaging"  />
+                <HeaderOption Icon={NotificationsIcon} title="Notification"  />
+                <HeaderOption avatar="https://i.pinimg.com/originals/32/3f/d8/323fd8b794e9c1ab4f52de7e6b018852.jpg" title="Me" />
                 {/* <HeaderOption title=""   Icon={SearchIcon}/> */}
             </div>            
         </div>
     )
 }
 
+console.log({HomeIcon});
 
 export default Header
